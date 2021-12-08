8a3a12caf0b2ce8a369260212ea0a36e97ca4144
Hardik Mehta <info@tallyecom.in>
2021-12-08 17:31:25 +0530

    Update index.js
    index.js updated

--- pages/index.js
+++ pages/index.js
@@ -352,23 +352,23 @@ const Index = () => {
               </Card.Section>
             )}
           </Card>
-          <Layout>
-            <Layout.Section>
-              <Card>
-                <Card.Section title="Developer">
-                  <Heading>See-D Solutions</Heading>
-                  <br />
-                  <p>5, Sai Prasad Building, 1st Floor,</p>
-                  <p>3rd MamletdarWadi, Malad (West),</p>
-                  <p>Mumbai - 400064., Maharashtra, India</p>
-                  <br />
-                  <p>Support : +919082048148</p>
-                  <br />
-                  <p>E-mail : info@tallyecom.in</p>
-                </Card.Section>
-              </Card>
-            </Layout.Section>
-          </Layout>
+        </Layout.Section>
+      </Layout>
+      <Layout>
+        <Layout.Section>
+          <Card>
+            <Card.Section title="Developer">
+              <Heading>See-D Solutions</Heading>
+              <br />
+              <p>5, Sai Prasad Building, 1st Floor,</p>
+              <p>3rd MamletdarWadi, Malad (West),</p>
+              <p>Mumbai - 400064., Maharashtra, India</p>
+              <br />
+              <p>Support : +919082048148</p>
+              <br />
+              <p>E-mail : info@tallyecom.in</p>
+            </Card.Section>
+          </Card>
         </Layout.Section>
       </Layout>
     </Page>
