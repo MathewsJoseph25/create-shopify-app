e669c07b7bfd78c1b35bfeb1f34bf16e4cf073f7
Hardik Mehta <info@tallyecom.in>
2021-12-08 23:19:16 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -156,7 +156,7 @@ const Index = () => {
             </Layout.Section>
           </>
         ) : null}
-        {result.length !== null ? (
+        {result.length ? (
           <>
             <Layout.Section>
               <Card>
