618618b826f2baa7f8d81c7e17b1d096831439dd
Hardik Mehta <info@tallyecom.in>
2021-12-09 14:23:07 +0530

    Create gdpr.js

---
+++ pages/gdpr.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import {
+  Form,
+  FormLayout,
+  TextField,
+  Button,
+  Layout,
+  Banner,
+  Page,
+  Card,
+  Heading,
+  List,
+  TextContainer,
+  DataTable,
+  Stack,
+  Collapsible,
+} from "@shopify/polaris";
+import Link from "next/link";
+
+export class gdpr extends Component {
+  render() {
+    return (
+      <>
+        <Page>
+          <Card>
+            <Card.Section title="Tallyecom.in Privacy Notice">
+              <Layout>
+                  <Layout.Section title="Tallyecom.in Privacy Notice">
+                  <Heading element="h1">Tallyecom.in Privacy Notice</Heading>
+                  <Heading element="h2">Disclaimer: In the event of any discrepancy or conflict, the English version will prevail over the translation.</Heading>
+                      <TextContainer>
+
+                      </TextContainer>
+                  </Layout.Section>
+              </Layout>
+            </Card.Section>
+          </Card>
+        </Page>
+      </>
+    );
+  }
+}
+
+export default gdpr;
