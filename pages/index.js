fc4074ed31cec8f601079fb8c776c739281eccef
Hardik Mehta <info@tallyecom.in>
2021-12-09 09:17:19 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -137,221 +137,225 @@ const Index = () => {
 
   return (
     <Page>
-      <Layout>
-        <Layout.Section>
-          <Banner>
-            <Heading element="h1">Simplified E-Commerce Accounting</Heading>
-            <p>
-              Create Products, Manage Customers, Track Orders, Keep Accutate
-              Records, Easy Statutory Compliances
-            </p>
-          </Banner>
-        </Layout.Section>
-        {serial ? (
-          <>
-            <Layout.Section>
-              <Heading element="h1">Serial Number : </Heading>
-              <p>{serial}</p>
-            </Layout.Section>
-          </>
-        ) : null}
-        {product || image || orderRec || orderDel || orderRet ? (
-          <>
-            <Layout.Section>
-              <Card>
-                <Heading element="h1">Processed Data</Heading>
-                {product ? (
-                  <>
-                    <Heading element="h3">Products Uploaded : </Heading>
-                    <p>{product}</p>
-                    <br />
-                  </>
-                ) : null}
-                {image ? (
-                  <>
-                    <Heading element="h3">Images Uploaded : </Heading>
-                    <p>{image}</p>
-                    <br />
-                  </>
-                ) : null}
-                {orderRec ? (
-                  <>
-                    <Heading element="h3">Orders Received : </Heading>
-                    <p>{orderRec}</p>
-                    <br />
-                  </>
-                ) : null}
-                {orderDel ? (
-                  <>
-                    <Heading element="h3">Orders Delivered : </Heading>
-                    <p>{orderDel}</p>
-                    <br />
-                  </>
-                ) : null}
-                {orderRec ? (
-                  <>
-                    <Heading element="h3">Orders Returned : </Heading>
-                    <p>{orderRet}</p>
-                    <br />
-                  </>
-                ) : null}
+      <Card>
+        <Layout>
+          <Layout.Section>
+            <Banner>
+              <Heading element="h1">Simplified E-Commerce Accounting</Heading>
+              <p>
+                Create Products, Manage Customers, Track Orders, Keep Accutate
+                Records, Easy Statutory Compliances
+              </p>
+            </Banner>
+          </Layout.Section>
+          {serial ? (
+            <>
+              <Layout.Section>
+                <Heading element="h1">Serial Number : </Heading>
+                <p>{serial}</p>
+              </Layout.Section>
+            </>
+          ) : null}
+          {product || image || orderRec || orderDel || orderRet ? (
+            <>
+              <Layout.Section>
+                <Card>
+                  <Heading element="h1">Processed Data</Heading>
+                  {product ? (
+                    <>
+                      <Heading element="h3">Products Uploaded : </Heading>
+                      <p>{product}</p>
+                      <br />
+                    </>
+                  ) : null}
+                  {image ? (
+                    <>
+                      <Heading element="h3">Images Uploaded : </Heading>
+                      <p>{image}</p>
+                      <br />
+                    </>
+                  ) : null}
+                  {orderRec ? (
+                    <>
+                      <Heading element="h3">Orders Received : </Heading>
+                      <p>{orderRec}</p>
+                      <br />
+                    </>
+                  ) : null}
+                  {orderDel ? (
+                    <>
+                      <Heading element="h3">Orders Delivered : </Heading>
+                      <p>{orderDel}</p>
+                      <br />
+                    </>
+                  ) : null}
+                  {orderRec ? (
+                    <>
+                      <Heading element="h3">Orders Returned : </Heading>
+                      <p>{orderRet}</p>
+                      <br />
+                    </>
+                  ) : null}
 
-                <Card sectioned>
-                  <Stack vertical>
-                    <Button
-                      onClick={handleToggle}
-                      ariaExpanded={open}
-                      ariaControls="basic-collapsible"
-                    >
-                      Show Processed Data
-                    </Button>
-                    <Collapsible
-                      open={open}
-                      id="basic-collapsible"
-                      transition={{
-                        duration: "500ms",
-                        timingFunction: "ease-in-out",
-                      }}
-                      expandOnPrint
-                    >
-                      <DataTable
-                        columnContentTypes={[
-                          // "string",
-                          "date",
-                          "string",
-                          "string",
-                          "string",
-                          "string",
-                          "string",
-                          "string",
-                        ]}
-                        headings={[
-                          // "id",
-                          "Date",
-                          "Type",
-                          "ProcessID",
-                          "Status",
-                          "URL",
-                          "SystemName",
-                          "IP",
-                        ]}
-                        rows={result}
-                      />
-                    </Collapsible>
-                  </Stack>
-                </Card>
-              </Card>
-            </Layout.Section>
-          </>
-        ) : null}
-        <Layout.Section id="features">
-          <Card title="Features">
-            <Card.Section>
-              <List type="bullet">
-                <List.Item>Scope of Duplication : 0</List.Item>
-                <List.Item>need to Import / Export Data : 0</List.Item>
-                <List.Item>
-                  Fully Automated Synchronisation of Orders & Tracking Status
-                </List.Item>
-                <List.Item>
-                  Create Stock Item / Group in Tally.ERP9 / Tally Prime with
-                  Images and post them as Single / Multi Variant Products on
-                  Shopify
-                </List.Item>
-                <List.Item>
-                  Order Process :
-                  <List type="number">
-                    <List.Item>
-                      Receive Orders in Tally.ERP9 / Tally Prime as soon as they
-                      are booked on Shopify
-                    </List.Item>
-                    <List.Item>
-                      Material dispatch entry is booked in Tally.ERP9 / Tally
-                      Prime along with courier details once the Material is
-                      Dispatched
-                    </List.Item>
-                    <List.Item>
-                      keep track of material with the link provided in the
-                      Tally.ERP9 / Tally Prime interface
-                    </List.Item>
-                    <List.Item>
-                      Sales Entry is booked with necessary adjustment entry for
-                      COD Partner / Payment Gateway.
-                    </List.Item>
-                    <List.Item>
-                      Easy reconciliation with COD Partner / Payment Gateway
-                    </List.Item>
-                    <List.Item>
-                      In case Material is returned undelivered / post delivery,
-                      Automated rejection note is duly processed
-                    </List.Item>
-                    <List.Item>
-                      Autmoated Reversal is done for COD partner / Payment
-                      Gateway
-                    </List.Item>
-                  </List>
-                </List.Item>
-              </List>
-            </Card.Section>
-            {serial ? null : (
-              <Card.Section>
-                <TextContainer>
-                  <a>
-                    This app requires additional setup and a TCP file compiled
-                    on your Tally Serial Number, Please fill up the form Below.
-                  </a>
-                </TextContainer>
-                <Form
-                  onSubmit={handleSubmitSerial}
-                  // preventDefault={true}
-                  title="Registration"
-                  // method="POST"
-                >
-                  <FormLayout>
-                    <Card>
-                      <Card.Section>
-                        <TextField
-                          value={serialNum}
-                          onChange={handleSerialChange}
-                          label="Tally Serial Number"
-                          type="number"
-                          maxlength={9}
-                          minlength={9}
-                          min="700000000"
-                          max="800000000"
+                  <Card sectioned>
+                    <Stack vertical>
+                      <Button
+                        onClick={handleToggle}
+                        ariaExpanded={open}
+                        ariaControls="basic-collapsible"
+                      >
+                        Show Processed Data
+                      </Button>
+                      <Collapsible
+                        open={open}
+                        id="basic-collapsible"
+                        transition={{
+                          duration: "500ms",
+                          timingFunction: "ease-in-out",
+                        }}
+                        expandOnPrint
+                      >
+                        <DataTable
+                          columnContentTypes={[
+                            // "string",
+                            "date",
+                            "string",
+                            "string",
+                            "string",
+                            "string",
+                            "string",
+                            "string",
+                          ]}
+                          headings={[
+                            // "id",
+                            "Date",
+                            "Type",
+                            "ProcessID",
+                            "Status",
+                            "URL",
+                            "SystemName",
+                            "IP",
+                          ]}
+                          rows={result}
                         />
-                      </Card.Section>
-                    </Card>
-                    <Card>
-                      <Card.Section>
-                        <Button primary={true} fullWidth={true} submit>
-                          Submit
-                        </Button>
-                      </Card.Section>
-                    </Card>
-                  </FormLayout>
-                </Form>
+                      </Collapsible>
+                    </Stack>
+                  </Card>
+                </Card>
+              </Layout.Section>
+            </>
+          ) : null}
+          <Layout.Section id="features">
+            <Card title="Features">
+              <Card.Section>
+                <List type="bullet">
+                  <List.Item>Scope of Duplication : 0</List.Item>
+                  <List.Item>need to Import / Export Data : 0</List.Item>
+                  <List.Item>
+                    Fully Automated Synchronisation of Orders & Tracking Status
+                  </List.Item>
+                  <List.Item>
+                    Create Stock Item / Group in Tally.ERP9 / Tally Prime with
+                    Images and post them as Single / Multi Variant Products on
+                    Shopify
+                  </List.Item>
+                  <List.Item>
+                    Order Process :
+                    <List type="number">
+                      <List.Item>
+                        Receive Orders in Tally.ERP9 / Tally Prime as soon as
+                        they are booked on Shopify
+                      </List.Item>
+                      <List.Item>
+                        Material dispatch entry is booked in Tally.ERP9 / Tally
+                        Prime along with courier details once the Material is
+                        Dispatched
+                      </List.Item>
+                      <List.Item>
+                        keep track of material with the link provided in the
+                        Tally.ERP9 / Tally Prime interface
+                      </List.Item>
+                      <List.Item>
+                        Sales Entry is booked with necessary adjustment entry
+                        for COD Partner / Payment Gateway.
+                      </List.Item>
+                      <List.Item>
+                        Easy reconciliation with COD Partner / Payment Gateway
+                      </List.Item>
+                      <List.Item>
+                        In case Material is returned undelivered / post
+                        delivery, Automated rejection note is duly processed
+                      </List.Item>
+                      <List.Item>
+                        Autmoated Reversal is done for COD partner / Payment
+                        Gateway
+                      </List.Item>
+                    </List>
+                  </List.Item>
+                </List>
+              </Card.Section>
+              {serial ? null : (
                 <Card.Section>
                   <TextContainer>
                     <a>
-                      Click on the button below and open the console to view the
-                      data returned from server using authenticated api call.{" "}
+                      This app requires additional setup and a TCP file compiled
+                      on your Tally Serial Number, Please fill up the form
+                      Below.
                     </a>
-                    <Link href={`/api2?shop=${window.shop}`}>
-                      <a>Another API Demo Page</a>
-                    </Link>
-                    <br />
-                    <Link href={`/introduction?shop=${window.shop}`}>
-                      <a>Introduction</a>
-                    </Link>
-                    <br />
                   </TextContainer>
+                  <Form
+                    onSubmit={handleSubmitSerial}
+                    // preventDefault={true}
+                    title="Registration"
+                    // method="POST"
+                  >
+                    <FormLayout>
+                      <Card>
+                        <Card.Section>
+                          <TextField
+                            value={serialNum}
+                            onChange={handleSerialChange}
+                            label="Tally Serial Number"
+                            type="number"
+                            maxlength={9}
+                            minlength={9}
+                            min="700000000"
+                            max="800000000"
+                          />
+                        </Card.Section>
+                      </Card>
+                      <Card>
+                        <Card.Section>
+                          <Button primary={true} fullWidth={true} submit>
+                            Submit
+                          </Button>
+                        </Card.Section>
+                      </Card>
+                    </FormLayout>
+                  </Form>
+                  <Card.Section>
+                    <TextContainer>
+                      <a>
+                        Click on the button below and open the console to view
+                        the data returned from server using authenticated api
+                        call.{" "}
+                      </a>
+                      <Link href={`/api2?shop=${window.shop}`}>
+                        <a>Another API Demo Page</a>
+                      </Link>
+                      <br />
+                      <Link href={`/introduction?shop=${window.shop}`}>
+                        <a>Introduction</a>
+                      </Link>
+                      <br />
+                    </TextContainer>
+                  </Card.Section>
                 </Card.Section>
-              </Card.Section>
-            )}
-          </Card>
-        </Layout.Section>
-      </Layout>
+              )}
+            </Card>
+          </Layout.Section>
+        </Layout>
+      </Card>
       <Card>
         <Card.Section title="Developer">
           <Layout>
