fb2a31f9b6863402f36fa881415bf7189c691bbd
Hardik Mehta <info@tallyecom.in>
2021-12-10 22:28:17 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,33 +6,10 @@ var apiKey;
 if (dev) apiKey = JSON.stringify(process.env.DEV_SHOPIFY_API_KEY);
 else apiKey = JSON.stringify(process.env.PROD_SHOPIFY_API_KEY);
 
-var x = document.URL
-
 module.exports = withCSS({
   webpack: (config) => {
     const env = { API_KEY: apiKey };
     config.plugins.push(new webpack.DefinePlugin(env));
     return config;
-  },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: "frame-ancestors https://cambridgetestshop.myshopify.com",
-          },
-          {
-            key: "Content-Security-Policy",
-            value: "frame-ancestors https://admin.myshopify.com",
-          },
-          {
-            key: "Content-Security-Policy",
-            value: "frame-ancestors " + x,
-          },
-        ],
-      },
-    ];
   },
 });
