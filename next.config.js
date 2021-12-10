5d35022e3651de3f03b222d843458bcd22d8c104
Hardik Mehta <info@tallyecom.in>
2021-12-10 19:46:41 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -19,7 +19,7 @@ module.exports = withCSS({
         headers: [
           {
             key: "Content-Security-Policy",
-            value: "frame-ancestors 'self' https://*.myshopify.com https://cambridgetestshop.myshopify.com https://admin.myshopify.com",
+            value: "frame-ancestors script-src 'self'; https://*.myshopify.com; https://cambridgetestshop.myshopify.com; https://admin.myshopify.com;",
           },
         ],
       },
