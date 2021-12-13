98d78057143ff1adec5c89b8ebbd210c5e572b11
Hardik Mehta <info@tallyecom.in>
2021-12-13 15:44:41 +0530

    Update customerdatarequest.js

--- server/routers/webhook/handlers/customerdatarequest.js
+++ server/routers/webhook/handlers/customerdatarequest.js
@@ -2,7 +2,9 @@ const Shop = require("../../../models/shop");
 //function to add Webhooks to the store and set data to the database
 const customerDataRequest = async (payload, shop) => {
   try {
-    let doc = await Shop.findOne(shop);
+    let doc = await Shop.findOne({
+      shop: shop.replace("https://", "").replace("http://", "").split(".")[0],
+    });
     return doc;
   } catch (error) {
     console.log("Error while Webhook - Shop Update ::", error);
