72ca907f7f6169636df30eeb623bd38fc12073ae
Hardik Mehta <info@tallyecom.in>
2021-12-18 20:33:17 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -289,7 +289,7 @@ const Index = () => {
                 id="ytplayer"
                 type="text/html"
                 width="100%"
-                height="100%"
+                // height="100%"
                 top="0"
                 left="0"
                 border="0"
