c11219dc88aa9063eef76d140fe8fc99344b0c6e
Hardik Mehta <info@tallyecom.in>
2021-12-18 22:12:18 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -285,7 +285,7 @@ const Index = () => {
             ) : null}
             <Layout.Section>
               <Heading element="h1">Implementation Steps :</Heading>
-              <ReactPlayer url="https://www.youtube.com/embed/9eZT2I_AOu4" />
+              <ReactPlayer url="https://www.youtube.com/embed/9eZT2I_AOu4?origin=https://tallyecomwithjwt.herokuapp.com/" />
               {/* <iframe
                 id="ytplayer"
                 type="text/html"
